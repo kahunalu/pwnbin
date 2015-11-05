a9b5e9ea801982b4caefd43567decd242afb20b7
MclarenL1 <mclaren1@uvic.ca>
2015-11-05 15:21:40 -0800

    First Commit

---
+++ pwnbin.py
@@ -0,0 +1,57 @@
+from bs4 import BeautifulSoup
+import urllib2
+import time
+import sys
+
+keywords = ['ssh', 'pass', 'key', 'token']
+root_url = 'http://pastebin.com'
+raw_url = 'http://pastebin.com/raw.php?i='
+has_passwords = []
+
+def main():
+	paste_list = set([])
+	time_out = False
+	length = 0
+
+	print "\nCrawling " + root_url
+
+	while True:
+		root_html = BeautifulSoup(fetch_page(root_url), 'html.parser')
+		for page in find_new_pages(root_html):
+			length = len(paste_list)
+			paste_list.add(page)
+			if len(paste_list) > length:
+				find_passwords(raw_url+page)
+			else:
+				time_out = True
+
+		if time_out:
+			time.sleep(2)
+
+		sys.stdout.write("\rCrawled total of %d Pages containing keywords %d" % (len(paste_list), len(has_passwords)))
+		sys.stdout.flush()
+
+def find_new_pages(root_html):
+	new_pastes = []
+
+	div = root_html.find('div', {'id': 'menu_2'})
+	ul = div.find('ul', {'class': 'right_menu'})
+	for li in ul.findChildren():
+		if li.find('a'):
+			new_pastes.append(str(li.find('a').get('href')).replace("/", ""))
+
+	return new_pastes
+
+def find_passwords(raw_url):
+	paste = fetch_page(raw_url)
+	for keyword in keywords:
+		if keyword in paste:
+			has_passwords.append("found " + keyword + " in " + raw_url)
+		break
+
+def fetch_page(page):
+	response = urllib2.urlopen(page)
+	return response.read()
+
+if __name__ == "__main__":
+	main()
