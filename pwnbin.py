5fefd3b93462a6333e458abb48f683a344c6251d
tristanlatr <tris.la.tr@gmail.com>
2020-07-22 10:37:53 -0400

    Add option "--no-sandbox

--- pwnbin.py
+++ pwnbin.py
@@ -53,7 +53,7 @@ def main(argv):
 
         # Avoid error https://bugs.chromium.org/p/chromedriver/issues/detail?id=2473
         options = webdriver.ChromeOptions()
-        # options.add_argument("--no-sandbox")
+        options.add_argument("--no-sandbox")
         options.add_argument("--disable-dev-shm-usage")
 
         driver = webdriver.Chrome(options=options)
